7b7a244e5de80d8625c669861d8e94501ee807fd
a0909130820-byte <a0909130820@gmail.com>
2026-05-26 04:06:29 +0800

    update backend api

--- backend.py
+++ backend.py
@@ -39,7 +39,7 @@ QDRANT_URL = os.getenv("QDRANT_URL")
 QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-COLLECTION_NAME = os.getenv("QDRANT_COLLECTION", "pig")
+COLLECTION_NAME = os.getenv("QDRANT_COLLECTION", "l2100_manuals")
 
 
 # =========================
